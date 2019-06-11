7c2f7efb396a9f9fb098ede5f2c9c652073cb640
eric <Eric355402@yahoo.com>
2019-06-10 20:04:00 -0600

    added mouseover and load event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const intro = document.querySelector('.intro h2');
+intro.addEventListener('mouseover', () => (intro.style.color = 'dodgerblue'));
+
+window.addEventListener('load', event => {
+  console.log('page is fully loaded');
+});
